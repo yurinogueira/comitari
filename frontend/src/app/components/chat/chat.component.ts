2cbe4a9147718359b8b80db7f9f75225ef13f390
Yuri Nogueira <personal@yurinogueira.dev.br>
2023-11-13 22:49:20 -0300

    chore: try to load messages

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {AfterViewInit, Component} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {ChatService} from "../../services/chat.service";
 import {MessageModel} from "../../models/message.model";
@@ -9,9 +9,11 @@ import {MessageModel} from "../../models/message.model";
     styleUrls: ["./chat.component.css"],
     providers: [ChatService]
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewInit {
 
     messages: MessageModel[] = []
+
+    private shouldCall: boolean = false;
 
     public messageForm: FormGroup = new FormGroup({
         msg: new FormControl('', [Validators.min(1)]),
@@ -23,18 +25,40 @@ export class ChatComponent {
         });
     }
 
+    loadMessages() {
+        this.shouldCall = false;
+
+        if (this.messages.length > 0) {
+            const lastMessage = this.messages[this.messages.length - 1];
+            this.chatService.listAfter(lastMessage.id).then(messages => {
+                this.messages.push(...messages);
+            });
+        } else {
+            this.chatService.list().then(messages => {
+                this.messages.push(...messages);
+            });
+        }
+    }
+
     submit() {
         if (this.messageForm.valid) {
             this.chatService.create(this.messageForm.value.msg).then(message => {
                 this.messageForm.reset();
                 if (message) {
-                    const lastMessage = this.messages[this.messages.length - 1];
-                    this.chatService.listAfter(lastMessage.id).then(messages => {
-                        this.messages.push(...messages);
-                    });
+                    this.loadMessages();
                 }
             });
         }
+    }
+
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            if (!this.shouldCall) {
+                this.shouldCall = true;
+            } else {
+                this.loadMessages();
+            }
+        }, 5000)
     }
 
 }
